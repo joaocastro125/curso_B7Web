36bf1ae152ee9881f47b26e4bfd3595990a20c9e
projeto <joaocastro_125@hotmail.com>
2021-07-23 13:26:48 -0300

    alteracao

--- modulo3/function.js
+++ modulo3/function.js
@@ -5,3 +5,34 @@ function alterar(){
 let informacao= alterar()
 
 console.log(informacao)
+
+// utilizando paramentros 
+//  quando usamos paramentros ele poder ser usada como variavel 
+//  ex: 
+
+// function alterar(titulo,campo){
+//     document.querySelector('#titulo').innerHTML=titulo
+//      document.querySelector('#campo').value=campo
+// }
+// // passa a informação aqui 
+// let informacao2= alterar("trocou o titulo","trocou o campo")
+
+// outro ex: 
+
+function alterar(x,y){
+    total=x+y
+    document.querySelector("#campo").value=total;
+}
+
+function alterar(x,y){
+    total=x+y
+    return total
+}
+
+let resultado=alterar(10,15)
+console.log(resultado)
+// outro ex:
+// passa a informação aqui 
+let informacao2= alterar("trocou o titulo","trocou o campo")
+
+
